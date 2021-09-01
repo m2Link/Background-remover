7b085f8680dda74da092dc8e9b907ce2ca17ffd1
MuFaz-TG <88883729+BXBotz@users.noreply.github.com>
2021-09-01 07:48:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import requests
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-UNSCREEN_API = os.environ.get("UNSCREEN_API", "")
+API = os.environ["REMOVEBG_API"]
 IMG_PATH = "./DOWNLOADS"
 
 FayasNoushad = Client(
@@ -20,59 +20,28 @@ FayasNoushad = Client(
 )
 
 START_TEXT = """
-Hello {}, I am a media background remover bot. Send me a photo I will send the photo without background.
+Hai {}, 
 
-Made by @FayasNoushad
-"""
-HELP_TEXT = """
-- Just send me a photo
-- I will download it
-- I will send the photo without background
+`I am a simple image background remover bot. Send me a photo I will send the photo without background.`
 
-Made by @FayasNoushad
+👲 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : [ʙx ʙᴏᴛᴢ](https://t.me/BX_Botz)
 """
-ABOUT_TEXT = """
-- **Bot :** `Backround Remover Bot`
-- **Creator :** [Fayas](https://telegram.me/TheFayas)
-- **Channel :** [Fayas Noushad](https://telegram.me/FayasNoushad)
-- **Source :** [Click here](https://github.com/FayasNoushad/Remove-BG-Bot/tree/main)
-- **Language :** [Python3](https://python.org)
-- **Library :** [Pyrogram](https://pyrogram.org)
-- **Server :** [Heroku](https://heroku.com)
-"""
+
 START_BUTTONS = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton('Channel', url='https://telegram.me/FayasNoushad'),
-        InlineKeyboardButton('Feedback', url='https://telegram.me/TheFayas')
-        ],[
-        InlineKeyboardButton('Help', callback_data='help'),
-        InlineKeyboardButton('About', callback_data='about'),
-        InlineKeyboardButton('Close', callback_data='close')
+        InlineKeyboardButton('Developer', url='https://telegram.me/HMTHMA')
         ]]
     )
-HELP_BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('Home', callback_data='home'),
-        InlineKeyboardButton('About', callback_data='about'),
-        InlineKeyboardButton('Close', callback_data='close')
-        ]]
-    )
-ABOUT_BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('Home', callback_data='home'),
-        InlineKeyboardButton('Help', callback_data='help'),
-        InlineKeyboardButton('Close', callback_data='close')
-        ]]
-    )
+
 ERROR_BUTTONS = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton('Help', callback_data='help'),
-        InlineKeyboardButton('Close', callback_data='close')
+        InlineKeyboardButton('🛠️ Help', callback_data='help'),
+        InlineKeyboardButton('Close 🔒', callback_data='close')
         ]]
     )
 BUTTONS = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton('Join Updates Channel', url='https://telegram.me/FayasNoushad')
+        InlineKeyboardButton('Developer', url='https://telegram.me/HMTHMA')
         ]]
     )
 
@@ -82,18 +51,6 @@ async def cb_data(bot, update):
         await update.message.edit_text(
             text=START_TEXT.format(update.from_user.mention),
             reply_markup=START_BUTTONS,
-            disable_web_page_preview=True
-        )
-    elif update.data == "help":
-        await update.message.edit_text(
-            text=HELP_TEXT,
-            reply_markup=HELP_BUTTONS,
-            disable_web_page_preview=True
-        )
-    elif update.data == "about":
-        await update.message.edit_text(
-            text=ABOUT_TEXT,
-            reply_markup=ABOUT_BUTTONS,
             disable_web_page_preview=True
         )
     else:
@@ -107,9 +64,9 @@ async def start(bot, update):
         reply_markup=START_BUTTONS
     )
 
-@FayasNoushad.on_message(filters.private & (filters.photo | filters.video | filters.document))
+@FayasNoushad.on_message(filters.private & (filters.photo | filters.document))
 async def remove_background(bot, update):
-    if not REMOVEBG_API:
+    if not API:
         await update.reply_text(
             text="Error :- Remove BG Api is error",
             quote=True,
@@ -119,21 +76,25 @@ async def remove_background(bot, update):
         return
     await update.reply_chat_action("typing")
     message = await update.reply_text(
-        text="Analysing",
+        text="Analysing Your IMAGE,Please Wait....⏳",
         quote=True,
         disable_web_page_preview=True
     )
-    if update and update.media:
-        new_file = PATH + "/" + str(update.from_user.id) + "/"
-        if update.photo or (update.document and "image" in update.document.mime_type):
-            file_name = new_file + "image.jpg"
-            new_file_name = new_file + "no_bg.png"
-            await update.download(file_name)
-            await message.edit_text(
-                text="Photo downloaded successfully. Now removing background.",
-                disable_web_page_preview=True
+    if (update and update.media and (update.photo or (update.document and "image" in update.document.mime_type))):
+        file_name = IMG_PATH + "/" + str(update.from_user.id) + "/" + "image.jpg"
+        new_file_name = IMG_PATH + "/" + str(update.from_user.id) + "/" + "no_bg.png"
+        await update.download(file_name)
+        await message.edit_text(
+            text="Photo downloaded successfully. Now removing background.",
+            disable_web_page_preview=True
+        )
+        try:
+            new_image = requests.post(
+                "https://api.remove.bg/v1.0/removebg",
+                files={"image_file": open(file_name, "rb")},
+                data={"size": "auto"},
+                headers={"X-Api-Key": API}
             )
-            new_image = removebg_image(file_name)
             if new_image.status_code == 200:
                 with open(f"{new_file_name}", "wb") as image:
                     image.write(new_image.content)
@@ -145,38 +106,27 @@ async def remove_background(bot, update):
                 )
                 return
             await update.reply_chat_action("upload_photo")
+            await update.reply_document(
+                document=new_file_name,
+                quote=True
+            )
+            await message.delete()
             try:
-                await update.reply_document(
-                    document=new_file_name,
-                    quote=True
-                )
-                await message.delete()
-                try:
-                    os.remove(file_name)
-                except:
-                    pass
-            except Exception as error:
-                print(error)
-                await message.edit_text(
-                    text="Something went wrong! May be API limits.",
-                    disable_web_page_preview=True,
-                    reply_markup=ERROR_BUTTONS
-                ) 
+                os.remove(file_name)
+            except:
+                pass
+        except Exception as error:
+            print(error)
+            await message.edit_text(
+                text="Something went wrong! May be API limits.",
+                disable_web_page_preview=True,
+                reply_markup=ERROR_BUTTONS
+            )
     else:
         await message.edit_text(
             text="Media not supported",
             disable_web_page_preview=True,
             reply_markup=ERROR_BUTTONS
         )
-
-
-def removebg_image(file):
-    return requests.post(
-        "https://api.remove.bg/v1.0/removebg",
-        files={"image_file": open(file_name, "rb")},
-        data={"size": "auto"},
-        headers={"X-Api-Key": REMOVEBG_API}
-    )
-
 
 FayasNoushad.run()
